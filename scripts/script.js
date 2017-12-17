877e77dff0858406f45d23284ee7db66ead2374e
AFknUsername <github@freiman.cc>
2017-12-16 21:48:17 -0500

    Adding dynamic option creation in dropdowns.

--- scripts/script.js
+++ scripts/script.js
@@ -7,11 +7,9 @@ function setup() {
 
 function loadCommon() {
     $.getJSON('./data/common.json', function(data) {
-        json = data;
-        createDropdown(data);
-    });
-}
-
-function createDropdown(data) {
-
+        //json = data;
+        for(let i = 0; i < data.length; i++) {
+            document.getElementById('conversionType').options[i] = new Option(Object.keys(data[i]));
+        }
+    })
 }
