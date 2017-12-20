46dbdbde62ce5874fe38c569bca917ac10f1c904
AFknUsername <github@freiman.cc>
2017-12-19 20:34:42 -0500

    added click event and added array.remove() method

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,14 @@ window.addEventListener('load', setup);
 function setup() {
     $(`#convert`).click(convert);
     $(`.weatherSwitch`).click(switchWeather);
+    $('#addItem').click(addItem);
     $(`#units`).change(populateUnits);
     loadConvertOptions();
     getWeather();
 }
+
+Array.prototype.remove = function (from, to) {
+    var rest = this.slice((to || from) + 1 || this.length);
+    this.length = from < 0 ? this.length + from : from;
+    return this.push.apply(this, rest);
+};
