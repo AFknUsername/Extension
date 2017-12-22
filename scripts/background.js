ea11dc944c918c8fc070ee7a9dbe73890a052d4e
AFknUsername <github@freiman.cc>
2017-12-21 22:23:43 -0500

    a

--- scripts/background.js
+++ scripts/background.js
@@ -6,16 +6,6 @@ function fetchIMOTD() {
         success: function (result) {
             let imageURL = `http://bing.com/${result['images'][0]['url']}`; 
             $('body').css({"background": `url(${imageURL}) no-repeat center`});
-            // $('body').css({})
         }
     });
 }
-//     let xhr = new XMLHttpRequest();
-//     xhr.open('GET', 'http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1', true);
-//     xhr.onreadystatechange = function() {
-//         if (xhr.readyState === 4) {
-//             console.log(xhr.responseText);
-//         }
-//     }
-//     xhr.send();
-// }
